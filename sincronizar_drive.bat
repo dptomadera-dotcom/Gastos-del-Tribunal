9658a2c47702d8f15c3a4f1e14314f7d144889ff
German Medina Castellano <gmedcas@gmail.com>
2026-06-27 23:27:21 +0100

    chore: actualizar ruta de origen en sincronizar_drive.bat a la carpeta Coder

--- sincronizar_drive.bat
+++ sincronizar_drive.bat
@@ -1,11 +1,11 @@
 @echo off
 title Sincronizador de Google Drive - Gastos del Tribunal
 echo =======================================================================
-echo Sincronizando codigo de C:\CODER a la unidad virtual G: (Google Drive Cloud)...
+echo Sincronizando codigo de C: a la unidad virtual G: (Google Drive Cloud)...
 echo =======================================================================
 echo.
 
-robocopy "C:\CODER\Gastos-del-Tribunal" "G:\Mi unidad\OPOSICIONES 2026\09_PRUEBAS\Gastos-del-Tribunal" /E /XD node_modules .git /XF output.txt sincronizar_drive.bat
+robocopy "C:\CODER\Gastos-del-Tribunal" "G:\Mi unidad\OPOSICIONES 2026\09_PRUEBAS\Gastos-del-Tribunal" /E /XD node_modules .git /XF output.txt
 
 echo.
 echo =======================================================================
